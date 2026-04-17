228517814b841e34bd9e941ef559aac1b3b4d360
DYBInh2k5 <binh.vd01500@sinhvien.hoasen.edu.vn>
2026-04-17 09:20:03 +0700

    Update run_local.ps1 script to launch the newly added API Gateway

--- Seminar/Microservices/run_local.ps1
+++ Seminar/Microservices/run_local.ps1
@@ -6,6 +6,7 @@ python -m venv venv
 
 Write-Host "2. Installing requirements..."
 pip install -r service1_user\requirements.txt
+pip install -r service3_gateway\requirements.txt
 
 Write-Host "3. Generating gRPC files..."
 python -m grpc_tools.protoc -I.\proto --python_out=.\service1_user --grpc_python_out=.\service1_user .\proto\service.proto
@@ -14,9 +15,15 @@ python -m grpc_tools.protoc -I.\proto --python_out=.\service2_order --grpc_pytho
 Write-Host "4. Starting Service 1 (User Service - Port 8001 & 50051) in background..."
 Start-Process -NoNewWindow -FilePath ".\venv\Scripts\python.exe" -ArgumentList "service1_user\main.py"
 
-Write-Host "Waiting 3 seconds for Service 1 to start..."
+Write-Host "5. Starting Service 2 (Order Service - Port 8002) in background..."
+$env:USER_SERVICE_URL="localhost:50051"
+Start-Process -NoNewWindow -FilePath ".\venv\Scripts\python.exe" -ArgumentList "service2_order\main.py"
+
+Write-Host "Waiting 3 seconds for backend services to start..."
 Start-Sleep -Seconds 3
 
-Write-Host "5. Starting Service 2 (Order Service - Port 8002). Press Ctrl+C to stop this one..."
-$env:USER_SERVICE_URL="localhost:50051"
-python service2_order\main.py
+Write-Host "6. Starting Service 3 (API Gateway - Port 8000). Press Ctrl+C to stop..."
+$env:ORDER_SERVICE_URL="http://localhost:8002"
+# Force Python encoding to handle UTF8 prints properly in Windows CMD
+$env:PYTHONUTF8=1
+python service3_gateway\main.py
